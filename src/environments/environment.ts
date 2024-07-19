39ad71a01a175a9504fa2249da13705da171b878
MLopes2023 <mlsilva.lopes@gmail.com>
2024-07-19 07:42:04 -0300

    Ajsutes docker e login

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  url_base: "http://127.0.0.1:5000",
-  url_externa: "http://127.0.0.1:5001"
+  url_base: "http://api-rest-freegames:5000",
+  url_externa: "http://api-rest-gateway-externa:5001"
 };
 
 /*
